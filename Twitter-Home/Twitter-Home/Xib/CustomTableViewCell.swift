3409013d7bf4ce5c0f0e0c2d73b2449871fb3a54
okateru0217 <okateru.0217@gmail.com>
2021-03-21 14:00:08 +0900

    Add:アイコン押下時の処理
    リツイート, いいねボタン押下時、アイコンの見た目を変える処理を追加

--- Twitter-Home/Twitter-Home/Xib/CustomTableViewCell.swift
+++ Twitter-Home/Twitter-Home/Xib/CustomTableViewCell.swift
@@ -11,20 +11,45 @@ class CustomTableViewCell: UITableViewCell {
     @IBOutlet weak var contributorIcon: UIImageView!
     @IBOutlet weak var contributorNameLabel: UILabel!
     @IBOutlet weak var tweetContentLabel: UILabel!
-    @IBOutlet weak var replyIcon: UIImageView!
-    @IBOutlet weak var reTweetIcon: UIImageView!
-    @IBOutlet weak var likeIcon: UIImageView!
-    @IBOutlet weak var shareIcon: UIImageView!
+    @IBOutlet weak var reTweetIcon: UIButton!
+    @IBOutlet weak var niceIcon: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
     
+    // リツイートボタン押下時の処理
+    var isReTweetIconColor = true
+    @IBAction func reTweetButton(_ sender: Any) {
+        if isReTweetIconColor {
+            reTweetIcon.tintColor = UIColor.systemGreen
+            isReTweetIconColor.toggle()
+        } else {
+            reTweetIcon.tintColor = UIColor.lightGray
+            isReTweetIconColor.toggle()
+        }
+    }
+    
+    // いいねボタン押下時の処理
+    var isNiceIconColor = true
+    @IBAction func niceButton(_ sender: Any) {
+        let heart = UIImage(systemName: "heart")
+        let heartFill = UIImage(systemName: "heart.fill")
+        if isNiceIconColor {
+            niceIcon.setImage(heartFill, for: .normal)
+            niceIcon.tintColor = UIColor.systemPink
+            isNiceIconColor.toggle()
+        } else {
+            niceIcon.setImage(heart, for: .normal)
+            niceIcon.tintColor = UIColor.lightGray
+            isNiceIconColor.toggle()
+        }
+    }
 }
