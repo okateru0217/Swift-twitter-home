86459fd629cac34de88e5d4839f4383e20b17fc8
okateru0217 <okateru.0217@gmail.com>
2021-03-21 10:24:40 +0900

    Add:Twitterホーム画面の見た目を作成

--- Twitter-Home/Twitter-Home/ViewController.swift
+++ Twitter-Home/Twitter-Home/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tweetTableView: UITableView!
+    @IBOutlet weak var postTweetButton: UIButton!
     
-    
-    let item = ["Cell 1\nCell 1\nCell 1\nCell 1", "Cell 2", "Cell 3", "Cell 4", "Cell 5"]
+    let item = ["Cell 1\nCell 1\nCell 1\nCell 1", "Cell 2", "Cell 3", "Cell 4", "Cell 5", "Cell 6", "Cell 7", "Cell 8", "Cell 9", "Cell 10", "Cell 11", "Cell 12",]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
         
-//        tweetTableView.estimatedRowHeight = 50
-//        tweetTableView.rowHeight = UITableView.automaticDimension
         // Do any additional setup after loading the view.
     }
     
